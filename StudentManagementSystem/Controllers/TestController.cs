5abb93527dcc244f3cce2e24d32b3031e5f3f69f
AvanishKumarTiwari <avanishtiwari266@gmail.com>
2026-02-21 23:35:26 +0530

    TEst controller added

---
+++ StudentManagementSystem/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentManagementSystem.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
